72ac05bdb4d083ec86963deff96df905bf10aa8d
kbrpronet <kaibrown2889@gmail.com>
2022-09-16 17:59:17 -0400

    added redux middleware to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
-//requirements
+//------requirements and dependencies---------//
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
-//
 const app = express()
+
+//redux dependencies
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
 //variables
 const port = 3000
 
-//middleware
+//--------MIDDLEWARE-------------//
 //body parser extracts the entire body portion of an incoming request
 app.use(bodyParser.json())
 app.use(
@@ -15,8 +20,12 @@ app.use(
         extended:true,
     })
 )
-
+//redux middleware
+const compilier = webpack(config)
+app.use(webpackDevMiddleware(compilier, {noInfo: true, publicPath: config.output.publicPath}))
+app.use(webpackHotMiddleware(compilier))
 //root endpoint
+
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
