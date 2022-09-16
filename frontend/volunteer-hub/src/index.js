65176ff533cd9972cbd19041279d7ae8746eb45f
kbrpronet <kaibrown2889@gmail.com>
2022-09-16 18:26:07 -0400

    set up redux middleware in index.js

--- frontend/volunteer-hub/src/index.js
+++ frontend/volunteer-hub/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {render} from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux'
+import App from './containers/App'
+import quotesApp from './reducers'
+import thunkMiddleware from 'redux-thunk'
+import api from './middleware/api'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, api)(createStore)
+let store = createStoreWithMiddleware(quotesApp)
+let rootElement = document.getElementById('root')
+
+render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+)
